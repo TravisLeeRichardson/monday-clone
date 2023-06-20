b7ab729d5e0b0deae18b65448e4961ee3ee185d9
Travis Richardson <tlrichar@gmail.com>
2023-06-20 13:11:01 -0400

    added backend and astra db

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,88 +1,65 @@
-import TicketCard from "../components/TicketCard";
+import TicketCard from '../components/TicketCard'
+import { useState, useEffect, useContext } from 'react'
+import axios from 'axios'
+import CategoriesContext from '../context'
 
 const Dashboard = () => {
+    const [tickets, setTickets] = useState(null)
+    const { categories, setCategories } = useContext(CategoriesContext)
 
-    const tickets = [
-        {
-            category: 'Q1 2022',
-            color: 'red',
-            title: 'NFT Safety 101 Video',
-            owner: 'Travis Richardson',
-            avatar: 'https://i.imgur.com/RPZfFtT.jpg',
-            status: 'done',
-            priority: 5,
-            progress: 40,
-            description: 'Make a video showcasing how to work with NFTs safely.',
-            timestamp: '2022-02-11T07:36:17+0000'
-        },
-        {
-            category: 'Q1 2022',
-            color: 'green',
-            title: 'Make a video about making a bot',
-            owner: 'Travis Richardson',
-            avatar: 'https://i.imgur.com/RPZfFtT.jpg',
-            status: 'working on it',
-            priority: 3,
-            progress: 20,
-            description: 'Make a video showcasing how to work with NFTs safely.',
-            timestamp: '2022-04-11T07:36:17+0000'
-        },
-        {
-            category: 'Q3 2022',
-            color: 'blue',
-            title: 'Build a bot',
-            owner: 'Travis Richardson',
-            avatar: 'https://i.imgur.com/RPZfFtT.jpg',
-            status: 'not started',
-            priority: 4,
-            progress: 30,
-            description: 'Make a video showcasing how to work with NFTs safely.',
-            timestamp: '2022-05-11T07:36:17+0000'
-        }
+    useEffect(async () => {
+        const response = await axios.get('http://localhost:8000/tickets')
+
+        //wasn't sure how to get the Documet Id with the object.. open to better suggestions
+        const dataObject = response.data.data
+
+        const arrayOfKeys = Object.keys(dataObject)
+        const arrayOfData = Object.keys(dataObject).map((key) => dataObject[key])
+        const formattedArray = []
+        arrayOfKeys.forEach((key, index) => {
+            const formmatedData = { ...arrayOfData[index] }
+            formmatedData['documentId'] = key
+            formattedArray.push(formmatedData)
+        })
+
+        setTickets(formattedArray)
+    }, [])
+
+    useEffect(() => {
+        setCategories([...new Set(tickets?.map(({ category }) => category))])
+    }, [tickets])
+
+    const colors = [
+        'rgb(255,179,186)',
+        'rgb(255,223,186)',
+        'rgb(255,255,186)',
+        'rgb(186,255,201)',
+        'rgb(186,225,255)',
     ]
 
-        //if more than 5 categories, all others will be same. we could change that later.
-        const colors = [
-            'rgb(255,179,186)',
-            'rgb(255,223,186)',
-            'rgb(255,255,186)',
-            'rgb(186,255,201)',
-            'rgb(186,255,255)'
-        ]
-
-    const uniqueCategories =  [
-        //if tickets exist, then loop through all the ticket items, and get the category and return it. checking for uniqueness and assign using Set (Set only stores the unique items)
-        ...new Set(tickets?.map(({category}) => category))
+    const uniqueCategories = [
+        ...new Set(tickets?.map(({ category }) => category)),
     ]
-    console.log(uniqueCategories)
+
     return (
-        /*This code is creating a dashboard for "My Projects" consisting of tickets grouped
-        by unique categories. It first creates a containing div with a class name of "dashboard"
-        and a header with the text "My Projects". It then creates a second div with a class name
-        of "ticket-container". Inside this div, it uses the "&&" operator to check if the "tickets"
-        array exists, and if it does, it maps over the "uniqueCategories" array, displaying each
-        unique category as an h3 heading. For each category, it then uses the "filter" method to
-        create a new array of tickets that match that category, and maps over that array to display
-        each ticket as a "TicketCard" component. The "id" property of each "TicketCard" is set to
-        the index of that ticket in the filtered array, and the "color" and "ticket" properties
-        are set based on the data for that ticket.*/
-        <div className ="dashboard">
+        <div className="dashboard">
             <h1>My Projects</h1>
             <div className="ticket-container">
-                {tickets && uniqueCategories?.map((uniqueCategory, categoryIndex) => (
-                    <div key={categoryIndex}>
-                        <h3>{uniqueCategory}</h3>
-                        {tickets.filter(ticket => ticket.category === uniqueCategory)
-                        .map((filteredTicket, _index) => (
-                        <TicketCard
-                            id= {_index}
-                            color={colors[categoryIndex] || colors[0]}
-                            ticket= {filteredTicket}
-                            />
-                        ))
-                    }
-                    </div>
-                ))}
+                {tickets &&
+                    uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+                        <div key={categoryIndex}>
+                            <h3>{uniqueCategory}</h3>
+                            {tickets
+                                .filter((ticket) => ticket.category === uniqueCategory)
+                                .map((filteredTicket, _index) => (
+                                    <TicketCard
+                                        id={_index}
+                                        color={colors[categoryIndex] || colors[0]}
+                                        ticket={filteredTicket}
+                                    />
+                                ))}
+                        </div>
+                    ))}
             </div>
         </div>
     )
--- src/pages/TicketPage.js
+++ src/pages/TicketPage.js
@@ -1,42 +1,75 @@
 import { useState, useEffect, useContext } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
-//import CategoriesContext from '../context'
+import CategoriesContext from '../context'
 
-const TicketPage = () => {
+const TicketPage = ({ editMode }) => {
 
     const [formData, setFormData] = useState({
         status: 'not started',
         progress: 0,
-        timestamp: new Date().toISOString()
+        timestamp: new Date().toISOString(),
     })
-    const editMode = false;
+    const { categories, setCategories } = useContext(CategoriesContext)
 
-    const handleSubmit = () => {
-        console.log('submitted')
-    }
+    const navigate = useNavigate()
+    let { id } = useParams()
+
     const handleChange = (e) => {
         const value = e.target.value
         const name = e.target.name
 
         setFormData((prevState) => ({
             ...prevState,
-                [name]: value
-        })
-        )
+            [name]: value,
+        }))
     }
 
-    const categories = ['test1', 'test2']
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (editMode) {
+            const response = await axios.put(`http://localhost:8000/tickets/${id}`, {
+                data: formData,
+            })
+            const success = response.status === 200
+            if (success) {
+                navigate('/')
+            }
+        }
+        if (!editMode) {
+            console.log('posting')
+            const response = await axios.post('http://localhost:8000/tickets', {
+                formData,
+            })
+            const success = response.status === 200
+            if (success) {
+                navigate('/')
+            }
+        }
+    }
 
-    console.log(formData)
+    const fetchData = async () => {
+        const response = await axios.get(`http://localhost:8000/tickets/${id}`)
+        console.log('AAAAAA', response)
+        setFormData(response.data.data)
+    }
 
+    useEffect(() => {
+        if (editMode) {
+            fetchData()
+        }
+    }, [])
+
+    console.log('EDITcategories', categories)
+    console.log('formData', formData.status)
+    console.log('formData.status', formData.status === 'stuck')
     return (
         <div className="ticket">
-            <h1> {editMode ? 'Update Your Ticket' : 'Create a Ticket'}</h1>
+            <h1>{editMode ? 'Update Your Ticket' : 'Create a Ticket'}</h1>
             <div className="ticket-container">
                 <form onSubmit={handleSubmit}>
                     <section>
-                        <label htmlfor="title">Title</label>
+                        <label htmlFor="title">Title</label>
                         <input
                             id="title"
                             name="title"
@@ -44,7 +77,7 @@ const TicketPage = () => {
                             onChange={handleChange}
                             required={true}
                             value={formData.title}
-                            />
+                        />
 
                         <label htmlFor="description">Description</label>
                         <input
@@ -55,7 +88,6 @@ const TicketPage = () => {
                             required={true}
                             value={formData.description}
                         />
-
 
                         <label>Category</label>
                         <select
@@ -68,18 +100,131 @@ const TicketPage = () => {
                             ))}
                         </select>
 
-
                         <label htmlFor="new-category">New Category</label>
                         <input
                             id="new-category"
                             name="category"
                             type="text"
                             onChange={handleChange}
-                            required={true}
                             value={formData.category}
                         />
 
+                        <label>Priority</label>
+                        <div className="multiple-input-container">
+                            <input
+                                id="priority-1"
+                                name="priority"
+                                type="radio"
+                                onChange={handleChange}
+                                value={1}
+                                checked={formData.priority == 1}
+                            />
+                            <label htmlFor="priority-1">1</label>
+                            <input
+                                id="priority-2"
+                                name="priority"
+                                type="radio"
+                                onChange={handleChange}
+                                value={2}
+                                checked={formData.priority == 2}
+                            />
+                            <label htmlFor="priority-2">2</label>
+                            <input
+                                id="priority-3"
+                                name="priority"
+                                type="radio"
+                                onChange={handleChange}
+                                value={3}
+                                checked={formData.priority == 3}
+                            />
+                            <label htmlFor="priority-3">3</label>
+                            <input
+                                id="priority-4"
+                                name="priority"
+                                type="radio"
+                                onChange={handleChange}
+                                value={4}
+                                checked={formData.priority == 4}
+                            />
+                            <label htmlFor="priority-4">4</label>
+                            <input
+                                id="priority-5"
+                                name="priority"
+                                type="radio"
+                                onChange={handleChange}
+                                value={5}
+                                checked={formData.priority == 5}
+                            />
+                            <label htmlFor="priority-5">5</label>
+                        </div>
 
+                        {editMode && (
+                            <>
+                                <input
+                                    type="range"
+                                    id="progress"
+                                    name="progress"
+                                    value={formData.progress}
+                                    min="0"
+                                    max="100"
+                                    onChange={handleChange}
+                                />
+                                <label htmlFor="progress">Progress</label>
+
+                                <label>Status</label>
+                                <select
+                                    name="status"
+                                    value={formData.status}
+                                    onChange={handleChange}
+                                >
+                                    <option selected={formData.status == 'done'} value="done">
+                                        Done
+                                    </option>
+                                    <option
+                                        selected={formData.status == 'working on it'}
+                                        value="working on it"
+                                    >
+                                        Working on it
+                                    </option>
+                                    <option selected={formData.status == 'stuck'} value="stuck">
+                                        Stuck
+                                    </option>
+                                    <option
+                                        selected={formData.status == 'not started'}
+                                        value="not started"
+                                    >
+                                        Not Started
+                                    </option>
+                                </select>
+                            </>
+                        )}
+
+                        <input type="submit" />
+                    </section>
+
+                    <section>
+                        <label htmlFor="owner">Owner</label>
+                        <input
+                            id="owner"
+                            name="owner"
+                            type="owner"
+                            onChange={handleChange}
+                            required={true}
+                            value={formData.owner}
+                        />
+
+                        <label htmlFor="avatar">Avatar</label>
+                        <input
+                            id="avatar"
+                            name="avatar"
+                            type="url"
+                            onChange={handleChange}
+                        />
+                        <div className="img-preview">
+                            {formData.avatar && (
+                                <img src={formData.avatar} alt="image preview" />
+                            )}
+                        </div>
                     </section>
                 </form>
             </div>
